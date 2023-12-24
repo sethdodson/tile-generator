24f61c4656ea3123ad52017eafcd2d950a21ffe9
SethAtHome <seth_dodson@hotmail.com>
2023-12-24 13:42:16 -0600

    Changed tests to use test class

--- tests/TilesetGeneratorTests.fs
+++ tests/TilesetGeneratorTests.fs
@@ -5,21 +5,21 @@ open Xunit
 open System.IO
 open System
 open TilesetGenerator
-open System.Reflection
 
+type TilesetGeneratorTests() =
 
-[<Fact>]
-let ``generateFromSourceImage generates a png given valid directories`` () =
-    // Arrange
-    // get three levels up from the current directory
-    let projectDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName
-    let sourceDirectoryPath = Path.Combine(projectDirectory, "source-images")
-    let sourceDirectory = new DirectoryInfo(sourceDirectoryPath)
-    let outputDirectoryPath = Path.Combine(projectDirectory, "tiles")  
-    let outputDirectory = new DirectoryInfo(outputDirectoryPath)
+    [<Fact>]
+    member _. ``generateFromSourceImage generates a png given valid directories`` () =
+        // Arrange
+        // get three levels up from the current directory
+        let projectDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName
+        let sourceDirectoryPath = Path.Combine(projectDirectory, "source-images")
+        let sourceDirectory = new DirectoryInfo(sourceDirectoryPath)
+        let outputDirectoryPath = Path.Combine(projectDirectory, "tiles")  
+        let outputDirectory = new DirectoryInfo(outputDirectoryPath)
 
-    // Act
-    generateFromSourceImage sourceDirectory outputDirectory
+        // Act
+        generateFromSourceImage sourceDirectory outputDirectory
 
-    // Assert
-    outputDirectory.GetFiles().Length.Should().Be(1)
+        // Assert
+        outputDirectory.GetFiles().Length.Should().Be(1)
