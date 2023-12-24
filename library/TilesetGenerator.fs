6111c2c5a2e0332808dc727e848b790577cd98cb
SethAtHome <seth_dodson@hotmail.com>
2023-12-24 11:26:21 -0600

    First working test

---
+++ library/TilesetGenerator.fs
@@ -0,0 +1,10 @@
+﻿module TilesetGenerator
+
+open System.IO
+open System.Drawing
+
+
+let generateFromSourceImage (sourceDirectory:DirectoryInfo) (outputDirectory:DirectoryInfo) =
+    let bitmap = new Bitmap(1, 1)  // Create a tiny bitmap
+    let outputPath = Path.Combine(outputDirectory.FullName, "generated.png")
+    bitmap.Save(outputPath, Imaging.ImageFormat.Png)
--- tests/TilesetGeneratorTests.fs
+++ tests/TilesetGeneratorTests.fs
@@ -1,8 +1,25 @@
 module TilesetGeneratorTests
 
-open System
+open Faqt
 open Xunit
+open System.IO
+open System
+open TilesetGenerator
+open System.Reflection
+
 
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``generateFromSourceImage generates a png given valid directories`` () =
+    // Arrange
+    // get three levels up from the current directory
+    let projectDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName
+    let sourceDirectoryPath = Path.Combine(projectDirectory, "source-images")
+    let sourceDirectory = new DirectoryInfo(sourceDirectoryPath)
+    let outputDirectoryPath = Path.Combine(projectDirectory, "tiles")  
+    let outputDirectory = new DirectoryInfo(outputDirectoryPath)
+
+    // Act
+    generateFromSourceImage sourceDirectory outputDirectory
+
+    // Assert
+    outputDirectory.GetFiles().Length.Should().Be(1)
