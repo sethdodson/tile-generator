fa270a224c69f0b11430a82caa642e853c0d25d1
SethAtHome <seth_dodson@hotmail.com>
2023-12-25 20:06:04 -0600

    Moved setup methods

--- tests/TilesetGeneratorTests.fs
+++ tests/TilesetGeneratorTests.fs
@@ -7,15 +7,19 @@ open System
 open TilesetGenerator
 
 type TilesetGeneratorTests() =
+    let projectDirectoryPath = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName
+    let sourceDirectoryPath = Path.Combine(projectDirectoryPath, "source-images")
+    let outputDirectoryPath = Path.Combine(projectDirectoryPath, "tiles")
+
+    interface IDisposable with
+        member _.Dispose() = ()
+
 
     [<Fact>]
     member _. ``generateFromSourceImage generates a png given valid directories`` () =
         // Arrange
-        // get three levels up from the current directory
-        let projectDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName
-        let sourceDirectoryPath = Path.Combine(projectDirectory, "source-images")
-        let sourceDirectory = new DirectoryInfo(sourceDirectoryPath)
-        let outputDirectoryPath = Path.Combine(projectDirectory, "tiles")  
+        // get three levels up from the current directory                
+        let sourceDirectory = new DirectoryInfo(sourceDirectoryPath)        
         let outputDirectory = new DirectoryInfo(outputDirectoryPath)
 
         // Act
