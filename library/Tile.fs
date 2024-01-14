6c65d595e2def21c0be4d215cf4145ba02042ed4
SethAtHome <seth_dodson@hotmail.com>
2024-01-13 22:52:09 -0600

    added methods to draw the bounding box and the tile

--- library/Tile.fs
+++ library/Tile.fs
@@ -11,7 +11,20 @@ type Tile = {
     BoundingBoxTopRight: Point
     BoundingBoxBottomLeft: Point
     BoundingBoxBottomRight: Point
-}
+} with 
+    member this.DrawBoundingBox(graphics: Graphics) =
+        use pen = new Pen(Color.Red, 1.0f)
+        graphics.DrawLine(pen, this.BoundingBoxTopLeft, this.BoundingBoxTopRight)
+        graphics.DrawLine(pen, this.BoundingBoxTopRight, this.BoundingBoxBottomRight)
+        graphics.DrawLine(pen, this.BoundingBoxBottomRight, this.BoundingBoxBottomLeft)
+        graphics.DrawLine(pen, this.BoundingBoxBottomLeft, this.BoundingBoxTopLeft)
+
+    member this.DrawTile(graphics: Graphics) =
+        use pen = new Pen(Color.Blue, 1.0f)
+        graphics.DrawLine(pen, this.Top, this.Right)
+        graphics.DrawLine(pen, this.Right, this.Bottom)
+        graphics.DrawLine(pen, this.Bottom, this.Left)
+        graphics.DrawLine(pen, this.Left, this.Top)
 
 let createTile (boundingBoxTopLeft: Point) (boundingBoxWidth: int) : Tile =
     let leftSide = boundingBoxTopLeft.X
