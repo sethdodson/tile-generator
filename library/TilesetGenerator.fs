8646d777b28afa38690f2e2249d48a7237da2d6c
SethAtHome <seth_dodson@hotmail.com>
2024-02-11 08:11:10 -0600

    refactored createTiles for clarity

--- library/TilesetGenerator.fs
+++ library/TilesetGenerator.fs
@@ -5,15 +5,27 @@ open System.Drawing
 open Tile
 
 
-let createTiles (numberOfTiles: int) (tileWidth: int) (tilesPerRow: int) =
-    let tileHeight = tileWidth / 2 // Isometric tiles are half as tall as they are wide
-    [for i in 0 .. numberOfTiles - 1 ->
-        let x = (i % tilesPerRow) * tileWidth
-        let y = (i / tilesPerRow) * tileHeight
-        let boundingBoxTopLeft = Point(x, y)
-        createTile boundingBoxTopLeft tileWidth]
+let createTiles numberOfTiles tileWidth tilesPerRow =
+    let tileHeight = tileWidth / 2
+
+    let calculateLeftSideX tileIndex = 
+        let columnIndex = tileIndex % tilesPerRow
+        columnIndex * tileWidth
+
+    let calculateTopSideY tileIndex =
+        let rowIndex = tileIndex / tilesPerRow
+        rowIndex * tileHeight
+
+    let calculateBoundingBoxTopLeft tileIndex = Point(calculateLeftSideX tileIndex, calculateTopSideY tileIndex)
+
+    let createTileForTileIndex tileIndex = 
+        let boundingBoxTopLeft = calculateBoundingBoxTopLeft tileIndex
+        createTile boundingBoxTopLeft tileWidth
+
+    [for i in 0 .. numberOfTiles - 1 -> createTileForTileIndex i]
 
 let generateFromSourceImage (sourceDirectory: DirectoryInfo) (outputDirectory: DirectoryInfo) (numberOfTiles: int) =
+
     let tileWidth, tileHeight = 256, 128  // Bounding box dimensions of each tile
     use bitmap = new Bitmap(tileWidth, tileHeight)
     use graphics = Graphics.FromImage(bitmap)
